cea820352464761ae372a62ef315f94819176806
2horang2 <cmcm1284@naver.com>
2023-06-13 14:59:20 +0900

    #1.1 query type

--- server.js
+++ server.js
@@ -2,7 +2,19 @@
 import { ApolloServer, gql } from "apollo-server";
 
 //const { ApolloServer, gql } = require("apollo-server");
-const server = new ApolloServer({});
-server.listen().then(({url})=>{
-    console.log(`Running on ${url}`)
+//gql은 미리 data type을 알고 있어야 된다
+//Define your GraphQL schema (GraphQL 스키마 정의)
+//모든 GraphQL 서버(Apollo Server 포함)는 스키마를 사용하여 클라이언트가 쿼리할 수 있는 데이터 구조를 정의합니다.
+//(스키마는 type definitions의 모음입니다.)
+const typeDefs = gql`
+  type Query {
+    text: String
+    hello: String
+  }
+`;
+
+const server = new ApolloServer({ typeDefs });
+
+server.listen().then(({ url }) => {
+  console.log(`Running on ${url}`);
 });
