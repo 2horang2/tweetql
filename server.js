774d7424c84201bb4ec9fde25cd64b2cfa8be105
2horang2 <cmcm1284@naver.com>
2023-06-13 15:08:12 +0900

    #1.2 scalar and root type

--- server.js
+++ server.js
@@ -7,9 +7,18 @@ import { ApolloServer, gql } from "apollo-server";
 //모든 GraphQL 서버(Apollo Server 포함)는 스키마를 사용하여 클라이언트가 쿼리할 수 있는 데이터 구조를 정의합니다.
 //(스키마는 type definitions의 모음입니다.)
 const typeDefs = gql`
+    type User {
+        id: ID
+        username: String
+    }
+    type Tweet{
+        id: ID
+        text: String
+        author : User
+    }
   type Query {
-    text: String
-    hello: String
+    allTweets : [Tweet]
+    tweet(id: ID): Tweet
   }
 `;
 
