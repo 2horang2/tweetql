228fbb939d64881a7bdcb949ee8daa2a67deec24
2horang2 <cmcm1284@naver.com>
2023-06-15 17:05:32 +0900

    #1.4 resolver

--- server.js
+++ server.js
@@ -2,20 +2,49 @@
 import { ApolloServer, gql } from "apollo-server";
 
 
+let tweets =[
+    {
+        id:"1",
+        text:"hello",
+        author: {
+            id:"1",
+            username: "jung"
+        }
+    },
+    {
+        id:"2",
+        text:"hi",
+    },
+];
+
+let users = [
+  {
+    id: "1",
+    firstName: "nico",
+    lastName: "las",
+  },
+  {
+    id: "2",
+    firstName: "Elon",
+    lastName: "Mask",
+  },
+];
+
 const typeDefs = gql`
     type User {
         id: ID!
-        username: String!
-        firstName: String
-        lastName: String
+        firstName: String!
+        lastName: String!
+        fullName: String!
     }
     type Tweet{
         id: ID!
         text: String!
-        author : User!
+        author : User
     }
     type Query {
         allTweets : [Tweet!]!
+        allUsers : [User!]!
         tweet(id: ID!): Tweet
     }
     type Mutation{
@@ -24,8 +53,46 @@ const typeDefs = gql`
     }
 `;
 
+const resolvers = {
+    Query: {
+      allTweets() {
+        return tweets;
+      },
+      tweet(root, { id }) {
+        return tweets.find((tweet) => tweet.id === id);
+      },
+      allUsers(){
+        return users;
+      },
+    },
+    Mutation: {
+        postTweet(_, { text, userId }) {
+          const newTweet = {
+            id: tweets.length + 1,
+            text,
+          };
+          tweets.push(newTweet);
+          return newTweet;
+        },
+        deleteTweet(_, { id }) {
+          const tweet = tweets.find((tweet) => tweet.id === id);
+          if (!tweet) return false;
+          tweets = tweets.filter((tweet) => tweet.id !== id);
+          return true;
+        },
+      },
+      User: {
+        //fullName(root){
+        fullName({firstName,lastName}){
+   
+          console.log(`${firstName}`);
+          console.log(`${lastName}`);
+          return `${firstName} ${lastName}`;
+        }
+      }
+  }
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
